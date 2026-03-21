bb156e6d17e7794a015ac252e96d7bf3392c28f1
J6kub <jakubb@uia.no>
2026-03-21 21:50:38 +0100

    Watercolor map

--- fastapiapp/static/app.js
+++ fastapiapp/static/app.js
@@ -4,4 +4,30 @@ L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
   attribution: "© OpenStreetMap contributors",
 }).addTo(map);
 
+const tonerLiteLayer = L.tileLayer('https://tiles.stadiamaps.com/tiles/stamen_watercolor/{z}/{x}/{y}.jpg', {
+    attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>',
+    subdomains: 'abcd',
+    minZoom: 1,
+    maxZoom: 20
+});
 
+tonerLiteLayer.addTo(map);
+
+const baseMaps = {
+    "Watercolor": tonerLiteLayer
+};
+
+const overlays = {};
+
+L.control.layers(baseMaps, overlays).addTo(map);
+
+let layerVisible = true;
+
+function toggleTonerLayer() {
+    if (layerVisible) {
+        map.removeLayer(tonerLiteLayer);
+    } else {
+        map.addLayer(tonerLiteLayer);
+    }
+    layerVisible = !layerVisible;
+}
